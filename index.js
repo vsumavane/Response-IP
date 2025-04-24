9a1c4bba73a32628ad05997f11b6faef7dbaa555
Vishal Umavane <vsumavane@outlook.com>
2025-04-24 16:03:05 +0530

    init

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    try {
+        const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+        res.send(ip);
+    } catch (error) {
+        res.status(500).send('Error retrieving IP address');
+    }
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
